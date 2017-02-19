be1bd6e25359493ecd416bfe66dc21712f7f0ed2
Mark Wiemer <mwwiemer@outlook.com>
2017-02-18 19:25:02 -0600

    Initial commit

---
+++ Assets/Hilbert.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hilbert : MonoBehaviour {
+
+    public int n;
+    public int initialRotation;
+    public const int
+        UP = 0,
+        RIGHT = 1,
+        DOWN = 2,
+        LEFT = 3;
+
+	// Use this for initialization
+	void Start () {
+        Generate(n, 0, 0, initialRotation);
+	}
+
+    void Generate(int n, int x, int y, float r)
+    {
+        if(r < 0) { r += 4; }
+        if(r >= 4) { r -= 4; }
+        if(n == 0) { return; }
+        int s = (int)Mathf.Pow(2, n - 1) - 1; // width of *smaller* curve
+        int o = s + 1;
+
+        int w = (int)Mathf.Pow(2, n) - 1; // width of *this* curve
+
+        Vector2 up = new Vector2(0, 1);
+        Vector2 right = new Vector2(1, 0);
+
+        if (r == UP)
+        {
+            Generate(n - 1, x, y + o, r); // top left
+            Generate(n - 1, x + o, y + o, r); // top right
+            Generate(n - 1, x, y, r + 1); // bottom left
+            Generate(n - 1, x + o, y, r - 1); // bottom right
+
+            Line(new Vector2(x, y + s), up); // left line
+            Line(new Vector2(x + s, y + o), right); // mid line
+            Line(new Vector2(x + w, y + o), -up); // right line
+        }
+
+        else if (r == RIGHT)
+        {
+            Generate(n - 1, x, y + o, r + 1); // top left
+            Generate(n - 1, x + o, y + o, r); // top right
+            Generate(n - 1, x, y, r - 1); // bottom left
+            Generate(n - 1, x + o, y, r); // bottom right
+
+            Line(new Vector2(x + o, y + w), -right); // top line
+            Line(new Vector2(x + o, y + o), -up); // mid line
+            Line(new Vector2(x + o, y), -right); // bottom line
+        }
+
+        else if (r == DOWN)
+        {
+            Generate(n - 1, x, y + o, r - 1); // top left
+            Generate(n - 1, x + o, y + o, r + 1); // top right
+            Generate(n - 1, x, y, r); // bottom left
+            Generate(n - 1, x + o, y, r); // bottom right
+
+            Line(new Vector2(x + w, y + s), up); // right line
+            Line(new Vector2(x + o, y + s), -right); // mid line
+            Line(new Vector2(x, y + s), up); // left line
+        }
+
+        else if(r == LEFT)
+        {
+            Generate(n - 1, x, y + o, r); // top left
+            Generate(n - 1, x + o, y + o, r - 1); // top right
+            Generate(n - 1, x, y, r); // bottom left
+            Generate(n - 1, x + o, y, r + 1); // bottom right
+
+            Line(new Vector2(x + s, y), right); // bottom line
+            Line(new Vector2(x + s, y + s), up); // mid line
+            Line(new Vector2(x + s, y + w), right); // top line
+        }
+        
+    }
+
+    void Line(Vector2 start, Vector2 length)
+    {
+        GameObject go = new GameObject();
+        LineRenderer r = go.AddComponent<LineRenderer>();
+        r.SetPositions(new Vector3[]{start, start + length});
+        r.SetWidth(0.1f, 0.1f);
+    }
+}
---
+++ Assets/PanAndZoom.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Makes the camera pan and zoom around the mouse.
+/// Right-click and drag to pan
+/// Scroll to zoom
+/// </summary>
+public class PanAndZoom : MonoBehaviour
+{
+    private Vector3 Origin;
+    private const float DELTA_SCROLL = 4,
+        MAX_SIZE = 128,
+        MIN_SIZE = 4;
+
+    void Update()
+    {
+        Zoom();
+        Pan();
+    }
+
+    void Pan()
+    {
+        if (Input.GetMouseButtonDown(1)) { Origin = MousePos(); }
+        else if (Input.GetMouseButton(1))
+        {
+            transform.position += Origin - MousePos();
+        }
+    }
+
+    void Zoom()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        float size = Camera.main.orthographicSize;
+        if (scroll > 0)
+        {
+            Camera.main.orthographicSize =
+                Mathf.Max(MIN_SIZE, size - DELTA_SCROLL);
+        }
+        else if (scroll < 0)
+        {
+            Camera.main.orthographicSize =
+                Mathf.Min(MAX_SIZE, size + DELTA_SCROLL);
+        }
+    }
+
+    public static Vector3 MousePos()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
